1e13c406eba73c47fda5265565b3b6704d2c76bb
Sol Aniel <solaaniel@gmail.com>
2025-05-28 11:50:20 -0300

    tigrinho

--- src/main/br/edu/ifsp/spo/java/cards/App.java
+++ src/main/br/edu/ifsp/spo/java/cards/App.java
@@ -3,50 +3,86 @@ package br.edu.ifsp.spo.java.cards;
 import br.edu.ifsp.spo.java.cards.itens.Carta;
 import br.edu.ifsp.spo.java.cards.itens.Naipe;
 import br.edu.ifsp.spo.java.cards.itens.Valor;
-import br.edu.ifsp.spo.java.cards.nucleo.Jogo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.text.MessageFormat;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        for (var naipe : Naipe.values()) {
-            for (var valor : Valor.values()) {
-                soltaTigrinho(naipe, valor);
+        soltaTigrinho();
+    }
+
+    private static void soltaTigrinho() {
+//        var carta = new Carta(naipe, valor);
+        var cartas = new ArrayList<Carta>(Arrays.asList());
+        var grupinho = new ArrayList<Carta>(Arrays.asList());
+        for (var suit : Naipe.values()) {
+            for (var rank : Valor.values()) {
+                cartas.add(new Carta(suit, rank));
+            }
+        }
+        for(int i = 0; i<=5; i++) {
+            grupinho.add(cartas.removeFirst());
+            if (i == 5 || cartas == null) {
+                tigrinhos(grupinho);
+                grupinho.clear();
+                i = 0;
             }
         }
     }
 
-    private static void soltaTigrinho(Naipe naipe, Valor valor) {
-        var carta = new Carta(naipe, valor);
+    private static void tigrinhos(ArrayList<Carta> cartas) {
+        var cardOffset = "      ";
 
-        var caminhoCompleto = String.format(
-                "%s/%s/%s.txt",
-                "/cards",
-                carta.getNaipe().toString().toLowerCase(),
-                carta.getValor().toString().toLowerCase());
+        var linhas = new String[8];
+        var linhaAtual = -1;
+
+        for( var carta: cartas){
+            var caminhoCompleto = String.format(
+                    "%s/%s/%s.txt",
+                    "/cards",
+                    carta.getNaipe().toString().toLowerCase(),
+                    carta.getValor().toString().toLowerCase());
 
         System.out.println(caminhoCompleto);
 
-        try{
-            InputStream inputStream = App.class.getResourceAsStream(caminhoCompleto);
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(App.class.getResourceAsStream(caminhoCompleto)))){
 
-            if(inputStream == null){
-                System.out.println("O arquivo não existe");
-            }else {
-                byte[] buffer = new byte[inputStream.available()];
-                inputStream.read(buffer);
+            String linha;
+            while((linha = reader.readLine()) != null) {
+                linhaAtual++;
 
-                var cartaComoString = new String(buffer);
-                System.out.println(cartaComoString);
+                if (linhas[linhaAtual] == null) {
+                    linhas[linhaAtual] = linha;
+                } else {
+                    linhas[linhaAtual] = linhas[linhaAtual] + cardOffset + linha;
+                }
             }
+            linhaAtual = -1;
+//            if(inputStream == null){
+//                System.out.println("O arquivo não existe");
+//            }else {
+//                byte[] buffer = new byte[inputStream.available()];
+//                inputStream.read(buffer);
+//
+//                var cartaComoString = new String(buffer);
+//                System.out.println(cartaComoString);
+//            }
 
         } catch (IOException exception){
             System.out.println(exception.getMessage());
         } catch (Exception exception){
             System.out.println("Exception: " +exception.getMessage() + exception.getClass().getName());
+        }
+        }
+
+        for(var linha: linhas){
+            System.out.println(linha);
         }
     }
 }
